2872460c956c7c04f3b41dc771f344835b399c86
Namrata <inboxtonamrata@gmail.com>
2020-07-31 06:18:34 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+$(document).ready(function(){
+$('.sidenav').sidenav();
+});
+  
+
+
+$(document).ready(function(){
+$('.tabs').tabs();
+});
+     
+
+// $(document).ready(function(){
+// $('.fixed-action-btn').floatingActionButton();
+// });
+
